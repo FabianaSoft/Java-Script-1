7ba314b653228e59b6c788a707bbc4de2bca3328
FabianaSoft <teixeira.fabiana1@gmail.com>
2024-04-05 16:07:11 -0300

    Iniciar HTML e JS

---
+++ script.js
@@ -0,0 +1,7 @@
+function soma( ) {
+    var n1 = parseFloat(document.getElementById('n1').value);
+    var n2 = parseFloat(document.getElementById('n2').value);
+    var n3 = parseFloat(document.getElementById('n3').value);
+    var resultado = n1 + n2 + n3;
+    document.getElementById('resultado').innerHTML = resultado;
+}
